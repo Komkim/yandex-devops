5a7ba1d65b19cda0319c8df80ce05c2fe2d546a1
Komkim <komkim@mail.ru>
2023-01-06 02:52:37 +0300

    repositories pattern

--- cmd/agent/internal/services/report.go
+++ cmd/agent/internal/services/report.go
@@ -1,14 +1,13 @@
 package services
 
 import (
-	transport "Komkim/go-musthave-devops-tpl/cmd/agent/pkg"
 	"Komkim/go-musthave-devops-tpl/cmd/agent/storage"
 	"runtime"
 )
 
-func Report(client transport.MyClient, stats runtime.MemStats, count int, rand float64) {
+func Report(storage storage.Sending, stats runtime.MemStats, count int, rand float64) {
 
 	m := myStatsConversionFromRuntimeMemStats(stats, count, rand)
 
-	storage.SendAll(client, m.convertToOneMetricSlice())
+	storage.SendAll(m.convertToOneMetricSlice())
 }
