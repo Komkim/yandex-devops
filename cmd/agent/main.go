4b26e51e1f3c197b41f67a8d3536b23da8e1566c
Komkim <komkim@mail.ru>
2023-01-25 11:59:48 +0300

    fix test

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	//cfg, err := config.InitAgent()
-	cfg, err := config.InitFlagAgent()
+	cfg, err := config.InitAgent()
+	//cfg, err := config.InitFlagAgent()
 	if err != nil {
 		log.Println(err)
 	}
