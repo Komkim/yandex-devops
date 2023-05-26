943749cbcef3b18d73474a8426760bafd4e1a4b0
Komkim <komkim@mail.ru>
2023-05-26 05:10:03 +0300

    fix test

--- storage/file/file_test.go
+++ storage/file/file_test.go
@@ -1,161 +1,160 @@
 package file
 
-//
-//import (
-//	"github.com/stretchr/testify/require"
-//	"testing"
-//	"yandex-devops/storage"
-//)
-//
-//func TestFileStorage_GetOne(t *testing.T) {
-//	var (
-//		req   = require.New(t)
-//		file  = getTestFileRepo()
-//		value = float64(3702784)
-//		delta = int64(4)
-//	)
-//	defer func() {
-//		file.producer.Close()
-//		file.consumer.Close()
-//	}()
-//
-//	err := file.producer.Cleaning()
-//	req.NoError(err)
-//
-//	metrics := []storage.Metrics{
-//		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
-//		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
-//	}
-//
-//	_, err = file.SetAll(metrics)
-//	req.NoError(err)
-//
-//	test := func(metric *storage.Metrics, isError bool) func(t *testing.T) {
-//		return func(t *testing.T) {
-//			actual, err := file.GetOne(metric.ID)
-//			if isError {
-//				req.Error(err)
-//			} else {
-//				req.NoError(err)
-//			}
-//			req.Equal(metric.ID, actual.ID)
-//			req.Equal(metric.MType, actual.MType)
-//			req.Equal(metric.Delta, actual.Delta)
-//			req.Equal(metric.Value, actual.Value)
-//			req.Equal(metric.Hash, actual.Hash)
-//		}
-//
-//	}
-//	t.Run("select type gauge", test(&metrics[0], false))
-//	//t.Run("select type counter", test(&metrics[1]))
-//}
-//
-//func TestFileStorage_GetAll(t *testing.T) {
-//	var (
-//		req   = require.New(t)
-//		file  = getTestFileRepo()
-//		value = float64(3702784)
-//		delta = int64(4)
-//	)
-//	defer func() {
-//		file.producer.Close()
-//		file.consumer.Close()
-//	}()
-//
-//	err := file.producer.Cleaning()
-//	req.NoError(err)
-//
-//	metrics := []storage.Metrics{
-//		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
-//		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
-//	}
-//
-//	_, err = file.SetAll(metrics)
-//	req.NoError(err)
-//
-//	test := func(metric *storage.Metrics, isError bool) func(t *testing.T) {
-//		return func(t *testing.T) {
-//			actual, err := file.GetAll()
-//			if isError {
-//				req.Error(err)
-//			} else {
-//				req.NoError(err)
-//			}
-//			req.Equal(metrics, actual)
-//		}
-//
-//	}
-//	t.Run("select type gauge", test(&metrics[0], false))
-//}
-//
-//func TestFileStorage_SetOne(t *testing.T) {
-//	var (
-//		req   = require.New(t)
-//		file  = getTestFileRepo()
-//		value = float64(3702784)
-//		delta = int64(4)
-//	)
-//	defer func() {
-//		file.producer.Close()
-//		file.consumer.Close()
-//	}()
-//
-//	err := file.producer.Cleaning()
-//	req.NoError(err)
-//
-//	metrics := []storage.Metrics{
-//		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
-//		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
-//	}
-//
-//	test := func(m storage.Metrics) func(t *testing.T) {
-//		return func(t *testing.T) {
-//			_, err := file.SetOne(m)
-//			req.NoError(err)
-//
-//			r, err := file.GetOne(m.ID)
-//			req.NoError(err)
-//
-//			req.Equal(m.ID, r.ID)
-//			req.Equal(m.MType, r.MType)
-//		}
-//
-//	}
-//	t.Run("insert type gauge", test(metrics[0]))
-//	t.Run("insert type counter", test(metrics[1]))
-//}
-//
-//func TestFileStorage_SetAll(t *testing.T) {
-//	var (
-//		req   = require.New(t)
-//		file  = getTestFileRepo()
-//		value = float64(3702784)
-//		delta = int64(4)
-//	)
-//	defer func() {
-//		file.producer.Close()
-//		file.consumer.Close()
-//	}()
-//
-//	err := file.producer.Cleaning()
-//	req.NoError(err)
-//
-//	metrics := []storage.Metrics{
-//		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
-//		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
-//	}
-//
-//	test := func(m []storage.Metrics) func(t *testing.T) {
-//		return func(t *testing.T) {
-//			_, err := file.SetAll(m)
-//			req.NoError(err)
-//
-//			r, err := file.GetAll()
-//			req.NoError(err)
-//
-//			req.Equal(m, r)
-//		}
-//
-//	}
-//	t.Run("select all", test(metrics))
-//}
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"yandex-devops/storage"
+)
+
+func TestFileStorage_GetOne(t *testing.T) {
+	var (
+		req   = require.New(t)
+		file  = getTestFileRepo()
+		value = float64(3702784)
+		delta = int64(4)
+	)
+	defer func() {
+		file.producer.Close()
+		file.consumer.Close()
+	}()
+
+	err := file.producer.Cleaning()
+	req.NoError(err)
+
+	metrics := []storage.Metrics{
+		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
+		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
+	}
+
+	_, err = file.SetAll(metrics)
+	req.NoError(err)
+
+	test := func(metric *storage.Metrics, isError bool) func(t *testing.T) {
+		return func(t *testing.T) {
+			actual, err := file.GetOne(metric.ID)
+			if isError {
+				req.Error(err)
+			} else {
+				req.NoError(err)
+			}
+			req.Equal(metric.ID, actual.ID)
+			req.Equal(metric.MType, actual.MType)
+			req.Equal(metric.Delta, actual.Delta)
+			req.Equal(metric.Value, actual.Value)
+			req.Equal(metric.Hash, actual.Hash)
+		}
+
+	}
+	t.Run("select type gauge", test(&metrics[0], false))
+	//t.Run("select type counter", test(&metrics[1]))
+}
+
+func TestFileStorage_GetAll(t *testing.T) {
+	var (
+		req   = require.New(t)
+		file  = getTestFileRepo()
+		value = float64(3702784)
+		delta = int64(4)
+	)
+	defer func() {
+		file.producer.Close()
+		file.consumer.Close()
+	}()
+
+	err := file.producer.Cleaning()
+	req.NoError(err)
+
+	metrics := []storage.Metrics{
+		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
+		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
+	}
+
+	_, err = file.SetAll(metrics)
+	req.NoError(err)
+
+	test := func(metric *storage.Metrics, isError bool) func(t *testing.T) {
+		return func(t *testing.T) {
+			actual, err := file.GetAll()
+			if isError {
+				req.Error(err)
+			} else {
+				req.NoError(err)
+			}
+			req.Equal(metrics, actual)
+		}
+
+	}
+	t.Run("select type gauge", test(&metrics[0], false))
+}
+
+func TestFileStorage_SetOne(t *testing.T) {
+	var (
+		req   = require.New(t)
+		file  = getTestFileRepo()
+		value = float64(3702784)
+		delta = int64(4)
+	)
+	defer func() {
+		file.producer.Close()
+		file.consumer.Close()
+	}()
+
+	err := file.producer.Cleaning()
+	req.NoError(err)
+
+	metrics := []storage.Metrics{
+		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
+		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
+	}
+
+	test := func(m storage.Metrics) func(t *testing.T) {
+		return func(t *testing.T) {
+			_, err := file.SetOne(m)
+			req.NoError(err)
+
+			r, err := file.GetOne(m.ID)
+			req.NoError(err)
+
+			req.Equal(m.ID, r.ID)
+			req.Equal(m.MType, r.MType)
+		}
+
+	}
+	t.Run("insert type gauge", test(metrics[0]))
+	t.Run("insert type counter", test(metrics[1]))
+}
+
+func TestFileStorage_SetAll(t *testing.T) {
+	var (
+		req   = require.New(t)
+		file  = getTestFileRepo()
+		value = float64(3702784)
+		delta = int64(4)
+	)
+	defer func() {
+		file.producer.Close()
+		file.consumer.Close()
+	}()
+
+	err := file.producer.Cleaning()
+	req.NoError(err)
+
+	metrics := []storage.Metrics{
+		{ID: "HeapSys", MType: "gauge", Value: &value, Hash: "e3ec1cae0b022f109fada933959833ee75a54c58900c6fe6eca8d70195df13e5"},
+		{ID: "PollCount", MType: "counter", Delta: &delta, Hash: "f2b586d3bd28e23820ebd4c0149104791821d5e25b96b2d241c383fd1e5b0668"},
+	}
+
+	test := func(m []storage.Metrics) func(t *testing.T) {
+		return func(t *testing.T) {
+			_, err := file.SetAll(m)
+			req.NoError(err)
+
+			r, err := file.GetAll()
+			req.NoError(err)
+
+			req.Equal(m, r)
+		}
+
+	}
+	t.Run("select all", test(metrics))
+}
