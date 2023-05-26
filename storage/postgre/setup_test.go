dc76590c12a6ba69236af01f8f0cf37d550638ad
Komkim <komkim@mail.ru>
2023-05-26 04:17:48 +0300

    fix test

--- storage/postgre/setup_test.go
+++ storage/postgre/setup_test.go
@@ -3,7 +3,6 @@ package postgresql
 import (
 	"context"
 	"github.com/stretchr/testify/require"
-	"github.com/vrischmann/envconfig"
 )
 
 const skipTestMessage = "Skip test. please up local database for this test"
@@ -11,18 +10,20 @@ const skipTestMessage = "Skip test. please up local database for this test"
 func getStorageRepo(req *require.Assertions) PostgreStorage {
 	ctx := context.Background()
 
-	type Config struct {
-		DatabaseDSN string `env:"DATABASE_URI" mapstructure:"databasedsn"`
-	}
-	type cfg struct {
-		Dsn *Config
-	}
+	//type Config struct {
+	//	DatabaseDSN string `env:"DATABASE_URI" mapstructure:"databasedsn"`
+	//}
+	//type cfg struct {
+	//	Dsn *Config
+	//}
+	//
+	//var c cfg
+	//err := envconfig.Init(&c)
+	//req.NoError(err)
+	//repo, err := New(ctx, c.Dsn.DatabaseDSN)
+	dsn := "postgres://postgres:changeme@localhost:5432/yandex?sslmode=disable"
+	repo, err := New(ctx, dsn)
 
-	var c cfg
-	err := envconfig.Init(&c)
-	req.NoError(err)
-
-	repo, err := New(ctx, c.Dsn.DatabaseDSN)
 	req.NoError(err)
 
 	return *repo
