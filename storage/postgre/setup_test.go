22c2d9d9d466ec7c93731fecdca43a2c30404766
Komkim <komkim@mail.ru>
2023-05-26 04:40:38 +0300

    fix test

--- storage/postgre/setup_test.go
+++ storage/postgre/setup_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/stretchr/testify/require"
 	"github.com/vrischmann/envconfig"
-	"yandex-devops/config"
 )
 
 const skipTestMessage = "Skip test. please up local database for this test"
@@ -12,24 +11,24 @@ const skipTestMessage = "Skip test. please up local database for this test"
 func getStorageRepo(req *require.Assertions) PostgreStorage {
 	ctx := context.Background()
 
-	//type Config struct {
-	//	DSN string `env:"DSN_DATABASE" mapstructure:"databasedsn"`
-	//}
+	type Config struct {
+		DSN string `env:"DSN_DATABASE" mapstructure:"databasedsn"`
+	}
 	//type cfg struct {
 	//	c *Config
 	//}
-	//
-	//var cc cfg
 
-	type cfg struct {
-		Dsn *config.Server
-	}
+	var c Config
 
-	var c cfg
+	//type cfg struct {
+	//	Dsn *config.Server
+	//}
+
+	//var c cfg
 	err := envconfig.Init(&c)
 	req.NoError(err)
 
-	repo, err := New(ctx, c.Dsn.DatabaseDSN)
+	repo, err := New(ctx, c.DSN)
 
 	req.NoError(err)
 
